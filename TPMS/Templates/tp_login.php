8ee0c94519ed3d9480097821a23179b21517921d
AnasElAbd28 <anasehabelabd12@gmail.com>
2023-06-13 13:39:53 +0800

    TP-basics added

--- TPMS/Templates/tp_login.php
+++ TPMS/Templates/tp_login.php
@@ -17,31 +17,30 @@ $email = validate($_POST['Email']);
 $password = validate($_POST['Password']);
 
 if(empty($email)){
-    header ("Location: login_page.php?error=Email is required");
+    header ("Location: tp_login_page.php?error=Email is required");
     exit();
 }elseif(empty($password)){
-    header ("Location: login_page.php?error=Password is required");
+    header ("Location: tp_login_page.php?error=Password is required");
     exit();
 }
 
-$sql = "SELECT * FROM user WHERE email ='$email' AND Password = '$password'";
+$sql = "SELECT * FROM training_providor WHERE TP_Email ='$email' AND TP_Password = '$password'";
 
 $result = mysqli_query($conn, $sql);
 
 if(mysqli_num_rows($result) === 1){
     $row = mysqli_fetch_assoc($result);
-    if($row['email'] === $email && $row['Password'] === $password){
+    if($row['TP_Email'] === $email && $row['TP_Password'] === $password){
         echo "logged in";
-        $_SESSION['email'] = $row['email'];
-        $_SESSION['Full_Name'] = $row['Full_Name'];
-        $_SESSION['User_ID'] = $row['User_ID'];
-        $_SESSION['DOB'] = $row['DOB'];
-        header("Location: landing.php");
+        $_SESSION['TP_Email'] = $row['TP_Email'];
+        $_SESSION['TP_Name'] = $row['TP_Name'];
+        $_SESSION['TP_ID'] = $row['TP_ID'];
+        header("Location: tp_dashboard.php");
         exit();
     }else {
-        header("Location: login_page.php?error=Incorrect Email or Password" );
+        header("Location: tp_login_page.php?error=Incorrect Email or Password" );
     }
 }else {
-    header("Location: login_page.php");
+    header("Location: tp_login_page.php");
     exit();
 }
