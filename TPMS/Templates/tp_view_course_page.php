c2127b927f8408deb2f70f178e9c4e929dc897c9
AnasElAbd28 <anasehabelabd12@gmail.com>
2023-06-14 18:06:19 +0800

    COMMIT

--- TPMS/Templates/tp_view_course_page.php
+++ TPMS/Templates/tp_view_course_page.php
@@ -20,7 +20,11 @@
 <body>
     <?php
     include 'db_conn.php';
+<<<<<<< Updated upstream
     session_start(); 
+=======
+    session_start() 
+>>>>>>> Stashed changes
     ?>
     <nav>
         <a href="#">
@@ -50,7 +54,11 @@
 
         <div id="courses-container">
         <?php 
+<<<<<<< Updated upstream
    $sql = "SELECT * FROM course WHERE TP_ID ='" . $_SESSION["TP_ID"] . "'";
+=======
+    $sql = "SELECT * FROM course WHERE TP_ID ='" . $_SESSION["TP_ID"] . "'";
+>>>>>>> Stashed changes
     $result = mysqli_query($conn, $sql);
     if ($result->num_rows > 0) {
       // Output data of each row
