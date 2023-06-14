cb380e5211e17f475e2c59e246286c799df0f1b1
AnasElAbd28 <anasehabelabd12@gmail.com>
2023-06-14 16:24:30 +0800

    eferf

---
+++ TPMS/Templates/tp_view_course_page.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/layout.css">
+    <link rel="stylesheet" href="../Styles/landing.css">
+    <link rel="stylesheet" href="../Styles/dashboard.css">
+    <link rel="stylesheet" href="../Styles/create_course.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <script src="https://kit.fontawesome.com/3704673904.js" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://unpkg.com/simplebar@5.3.0/dist/simplebar.min.css" />
+<script src="https://unpkg.com/simplebar@5.3.0/dist/simplebar.min.js"></script>
+
+    <title>Create Course</title>
+</head>
+<body>
+    <nav>
+        <a href="#">
+            <h2>Pygmalion</h2>
+        </a>
+        <div>
+            <input class="search" type="text" name="" id="">
+            <i class="fa-solid fa-magnifying-glass" style="color: #f1f2f4;"></i>
+        </div>
+        <div>
+            <ul class="nav-links">
+                <li><a href="#">My courses</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+            
+        </div>
+        
+        <div class="burger">
+            <div class="l1"></div>
+            <div class="l2"></div>
+            <div class="l3"></div>
+            
+        </div>
+    </nav>
+    <div class="all-content">
+    <main>
+
+        
+        
+    </main>
+    <footer>
+
+    </footer>
+
+    <script src="../Javascript/app.js"></script>
+    <script src="../Javascript/landing.js"></script>
+</body>
+</html>
