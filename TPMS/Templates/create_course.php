8256346360c5dc576840d16e6fb6d53c4badc535
AnasElAbd28 <anasehabelabd12@gmail.com>
2023-06-14 16:04:31 +0800

    I added the course owner to course

--- TPMS/Templates/create_course.php
+++ TPMS/Templates/create_course.php
@@ -1,5 +1,6 @@
 <?php
 include "db_conn.php";
+session_start();
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
@@ -11,9 +12,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $course_start = $_POST["start-date"];
     $course_end = $_POST["end-date"];
     $course_category = $_POST["category"];
+    $course_owner = $_SESSION['TP_ID'];
 
     // Prepare and execute the SQL query to insert the data into the table
-    $sql = "INSERT INTO course (Course_Img, Course_Title, Course_Description, Course_Price, Course_Start, Course_End, Course_Category) VALUES ('$course_img', '$course_title', '$course_description', '$course_price', '$course_start' , '$course_end' , '$course_category')";
+    $sql = "INSERT INTO course (Course_Img, Course_Title, Course_Description, Course_Price, Course_Start, Course_End, Course_Category, TP_ID) VALUES ('$course_img', '$course_title', '$course_description', '$course_price', '$course_start' , '$course_end' , '$course_category' , '$course_owner')";
     if ($conn->query($sql) === TRUE) {
         header("Location: tp_dashboard.php");
         exit; 
