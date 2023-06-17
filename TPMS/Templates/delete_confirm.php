2f59608b8f54c1d46473740c40c166c4d6656dba
AnasElAbd28 <anasehabelabd12@gmail.com>
2023-06-18 03:58:45 +0800

    delete course done

---
+++ TPMS/Templates/delete_confirm.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/layout.css">
+  
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <script src="https://kit.fontawesome.com/3704673904.js" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://unpkg.com/simplebar@5.3.0/dist/simplebar.min.css" />
+<script src="https://unpkg.com/simplebar@5.3.0/dist/simplebar.min.js"></script>
+
+    <title>Landing Page</title>
+</head>
+<body>
+   
+    <div class="all-content">
+    <main>
+   <h4>are you sure you want to delete?</h4>
+   <button>Yes</button>
+   <button>No</button>
+        
+    </main>
+    <footer>
+
+    </footer>
+
+    <script src="../Javascript/app.js"></script>
+    <script src="../Javascript/landing.js"></script>
+</body>
+</html>
