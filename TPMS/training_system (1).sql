2799bbafb230917f4582a00a7b930db1bd7a06d0
Taha Al-Hariri <78170491+1201301923@users.noreply.github.com>
2023-06-25 22:47:39 +0800

    Update training_system (1).sql

--- TPMS/training_system (1).sql
+++ TPMS/training_system (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2023 at 06:33 PM
+-- Generation Time: Jun 25, 2023 at 04:46 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -96,7 +96,34 @@ INSERT INTO `user` (`User_ID`, `Full_Name`, `email`, `Password`, `DOB`, `Gender`
 (4, 'BodeSlayer', 'bode@gmail.com', 'Zalabia12', '2023-06-25', 'male'),
 (5, '3omda', '3omda@gmail.com', '3omda', '2023-06-12', 'male'),
 (6, 'Omar', 'Omar@gmail.com', '1234567', '2023-05-28', 'male'),
-(7, 'Anas', 'Anas@de7k.com', '12345', '2023-06-23', 'male');
+(7, 'Anas', 'Anas@de7k.com', '12345', '2023-06-23', 'male'),
+(8, 'TAHA MOHAMMED TAHA AL-HARIRI', 'tahaalhariri2@gmail.com', 'Taha1234', '2002-07-24', 'male');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_course`
+--
+
+CREATE TABLE `user_course` (
+  `UserCourse_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Course_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_course`
+--
+
+INSERT INTO `user_course` (`UserCourse_ID`, `User_ID`, `Course_ID`) VALUES
+(9, 8, 3),
+(10, 8, 7),
+(11, 8, 10),
+(13, 1, 6),
+(14, 1, 7),
+(15, 1, 3),
+(16, 1, 3),
+(17, 3, 10);
 
 --
 -- Indexes for dumped tables
@@ -122,6 +149,14 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`User_ID`);
 
 --
+-- Indexes for table `user_course`
+--
+ALTER TABLE `user_course`
+  ADD PRIMARY KEY (`UserCourse_ID`),
+  ADD KEY `User_ID` (`User_ID`),
+  ADD KEY `Course_ID` (`Course_ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -141,7 +176,13 @@ ALTER TABLE `training_providor`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_course`
+--
+ALTER TABLE `user_course`
+  MODIFY `UserCourse_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
@@ -152,6 +193,13 @@ ALTER TABLE `user`
 --
 ALTER TABLE `course`
   ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`TP_ID`) REFERENCES `training_providor` (`TP_ID`);
+
+--
+-- Constraints for table `user_course`
+--
+ALTER TABLE `user_course`
+  ADD CONSTRAINT `user_course_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`),
+  ADD CONSTRAINT `user_course_ibfk_2` FOREIGN KEY (`Course_ID`) REFERENCES `course` (`Course_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
